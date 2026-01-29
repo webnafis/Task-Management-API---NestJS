233b0b1d6dd113e62ccc0df85e6516e7fd68e32b
Nafis Shahriar <134604973+webnafis@users.noreply.github.com>
2026-01-29 18:48:59 +0600

    Configure ValidationPipe with whitelist option
    Updated ValidationPipe to use whitelist option.

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   // app.enableCors();
-  app.useGlobalPipes(new ValidationPipe()); //pipe is used diff ways
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // forbidNonWhitelisted: true,
+    }),
+  ); //pipe is used diff ways
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`Application listening on port ${3000}`);
